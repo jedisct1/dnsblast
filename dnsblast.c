d84dce117e3fd4236df839bc94635926d2468e4a
Frank Denis <github@pureftpd.org>
2012-05-14 19:57:47 -0700

    Fix encode_name()

--- dnsblast.c
+++ dnsblast.c
@@ -55,7 +55,7 @@ encode_name(unsigned char ** const encoded_ptr, size_t encoded_size,
     assert(encoded_size > (size_t) 0U);
     encoded_size--;
     for (;;) {
-        name_current_pos = find_name_component_len(name);
+        name_current_pos = find_name_component_len(name_current);
         if (name_current_pos == EOF ||
             encoded_size <= (size_t) name_current_pos) {
             return -1;
